1f1dc00c58c89eb64098b989639c2caad9fcf46b
iChris <christianjoseph473@gmail.com>
2023-06-06 19:56:55 +0100

    made it better

--- app.js
+++ app.js
@@ -11,3 +11,22 @@ window.addEventListener('load', function() {
     document.querySelector('body').classList.remove('centerBody');
     AOS.init();
 });
+
+const arrowRight = document.querySelector('.arrowRight');
+const arrowLeft = document.querySelector('.arrowLeft');
+const roadCont = document.querySelector('.roadCont');
+const roadClass = document.querySelector('.roadClass');
+
+arrowLeft.addEventListener('click', () => {
+    roadCont.scroll({
+        left: roadCont.scrollLeft - roadClass.scrollWidth,
+        behavior: 'smooth'
+    })
+})
+arrowRight.addEventListener('click', () => {
+    roadCont.scroll({
+        left: roadCont.scrollLeft + roadClass.scrollWidth,
+        behavior: 'smooth'
+    })
+})
+
