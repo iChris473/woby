9a9c35aaa4783d1ba7f26576923aaf86391499a4
iChris <christianjoseph473@gmail.com>
2023-06-11 16:01:23 +0100

    added img

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ window.addEventListener('load', function() {
     // Hide the loading animation
     var loadingAnimation = document.getElementById('loading-animation');
     loadingAnimation.style.display = 'none';
-  
+
     // Show the page content
     var pageContent = document.getElementById('page-content');
     pageContent.style.display = '';
